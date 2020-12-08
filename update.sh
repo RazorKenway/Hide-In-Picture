a6b6738661dd8c67c97da84e5695a30f55bca09a
Razor Kenway <razorkenway.98@gmail.com>
2020-12-09 00:57:51 +0530

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Coding by Razor Kenway (SL Android ) Sri lanka
+#Admin at SL Coders
+#Owner at SL Android
+
+
+
+rm -rf Hide-In-Picture 
+figlet Updating | lolcat
+git clone https://github.com/RazorKenway/Hide-In-Picture.git
+echo -e "\033[1;33m    Update Was Successfully Installed "
+echo -e "\033[1;33m    Tool By  Razor Kenway (SL Android ) Sri lanka')
+sleep 3
+cd Hide-In-Picture
+bash hide.sh
